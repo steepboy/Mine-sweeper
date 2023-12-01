19c462516dda570c837101338464ecee3208be61
stipashka <stepankv007@gmail.com>
2023-12-01 16:12:18 +0200

    хуйни настрочил

---
+++ not_main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+window = tk.Tk()
+
+row_num = 10
+col_num = 10
+
+button_list = []
+for i in range(row_num):
+    temp = []
+    for j in range(col_num):
+        cell = tk.Button(window, width = 3)
+        temp.append(cell)
+    button_list.append(temp)
+
+for row in button_list:
+    print(row)
+
+for i in range(row_num):
+    for j in range(col_num):
+        cell = button_list[i][j]
+        cell.grid(row=i, column=j)
+
+window.mainloop()
