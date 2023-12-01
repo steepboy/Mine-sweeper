aa38da9033a1633c67803fa42f95202a72f98891
yiski <nikbustrov@gmail.com>
2023-12-01 16:12:58 +0200

    Гей секс

---
+++ game.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+window = tk.Tk()
+row = 5
+columns = 5
+
+buttons = []
+for i in range(row):
+    temp = []
+    for j in range(columns):
+        btn = tk.Button(window, width=3, font='Calibri 15 bold')
+        temp.append(btn)
+    buttons.append(temp)
+
+for row_btn in buttons:
+    print(row_btn)
+
+for i in range(row):
+    for j in range(columns):
+        btn = buttons[i][j]
+        btn.grid(row=i, column=j)
+
+window.mainloop()
---
+++ menu.py
@@ -0,0 +1,9 @@
+import tkinter
+
+root = tkinter.Tk()
+root['bg'] = '#fafafa'
+root.title('Сапер')
+root.wm_attributes('-alpha', 1)
+root.geometry('450x450')
+root.mainloop()
+# 18x14
