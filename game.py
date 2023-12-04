2fe1f2400fbbb3a1fd0174a19e550a1fffdade11
yiski <nikbustrov@gmail.com>
2023-12-04 11:00:03 +0200

    Menu alfa version, add working start button end quit

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+
 window = tk.Tk()
+window.title("Гра")
 row = 5
 columns = 5
 
--- menu.py
+++ menu.py
@@ -1,9 +1,47 @@
-import tkinter
+import tkinter as tk
+import subprocess
+import time
 
-root = tkinter.Tk()
-root['bg'] = '#fafafa'
-root.title('Сапер')
-root.wm_attributes('-alpha', 1)
-root.geometry('450x450')
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    x = (window.winfo_screenwidth() // 2) - (width // 2)
+    y = (window.winfo_screenheight() // 2) - (height // 1)
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+def start_game():
+    print("Старт")
+    process = subprocess.Popen(["python", "launcher.py"])
+    process.wait()
+    time.sleep(0.3)
+    root.destroy()
+
+def open_settings():
+    print("Настройки")
+
+def quit_game():
+    root.quit()
+
+root = tk.Tk()
+root.title("САПЕР")
+root.geometry("300x300")
+root.resizable(width=False, height=False)
+
+def create_main_screen():
+    leb = tk.Label(root, text="САПЕР", font=("Arial", 30), fg="blue", height=2)
+    leb.pack()
+
+    start_button = tk.Button(root, text="Старт", command=start_game, height=2, width=10)
+    start_button.pack()
+
+    settings_button = tk.Button(root, text="Настройки", command=open_settings, height=2, width=10)
+    settings_button.pack()
+
+    quit_button = tk.Button(root, text="Выход", command=quit_game, height=2, width=10)
+    quit_button.pack()
+
+center_window(root)
+create_main_screen()
+
 root.mainloop()
-# 18x14
